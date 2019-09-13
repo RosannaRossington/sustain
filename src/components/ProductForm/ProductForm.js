57a6a03bd0958a970b22073dedab23ebbbc2e30c
Rosanna Rossington <rosanna.rossington@ynap.com>
2019-09-13 17:25:14 +0100

    chain requests

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
-
+//https://stackoverflow.com/questions/47956521/chain-requests-so-i-can-use-data-from-my-first-request-in-my-second-one
 //controlled component using event handlers
 export class ProductForm extends PureComponent {
   constructor(props) {
@@ -22,46 +22,65 @@ export class ProductForm extends PureComponent {
     this.setState({ value: finalProductNumber });
   }
 
-  handleSubmit(event, state) {
+  handleSubmit(event) {
     const productId = JSON.stringify(this.state.value);
-    const url = `https://ecomm.ynap.biz/yoox/ton/search/resources/store/theoutnet_GB/productview/${productId}`;
+    const productUrl = `https://ecomm.ynap.biz/yoox/ton/search/resources/store/theoutnet_GB/productview/${productId}`;
+    const impactUrl = "../impact.json";
+
     axios
-      .get(url, {
+      .get(productUrl, {
         headers: {
           "x-ibm-client-Id": "705b890d-fdb9-4867-a392-331c2fb86e19"
         }
       })
       .then(response => {
         // create an array of products only with relevant data
-        const newProducts = response.data.products.map(p => {
-          return [
-            p.name,
-            p.designerName,
-            p.productColours[0].sKUs[0].composition
-          ];
+        const productMaterial = response.data.products.map(p => {
+          return [p.productColours[0].sKUs[0].composition];
         });
 
         // create a new "State" object without mutating
         // the original State object.
         const newState = Object.assign({}, this.state, {
-          products: newProducts
+          products: productMaterial
         });
 
         // store the new state object in the component's state
         this.setState(newState);
       })
       .catch(error => console.log(error));
+
+    // const getMaterialImpact = () => {
+    //   axios
+    //   .get(impactUrl)
+    //   .then(response => {
+    //     //check productId matches material_name if so return impact object
+    //     const impacts = response.data.impacts.map(m => {
+    //       if(m.material_name === productId){
+    //         return [
+    //           m.physical_waste_total
+    //         ];
+    //       }
+    //     }
+    //     const impactState = Object.assign({}, this.state, {
+    //       impacts: newImpacts
+    //     });
+
+    //     this.setState(impactState);
+    //   })
+    //   .catch(error => console.log(error));
+    // };
     event.preventDefault();
   }
 
-  componentDidMount() {}
+  //state has changed and now includes the material so now want to make a call to return imapct of the material
+  componentDidUpdate() {}
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <div>{this.state.products} </div>
-        <div>"Product is"{this.state.products[3]} </div>
-
+        <div>Material:{this.state.products}</div>
+        <div>This material impacts:</div>
         <label>
           Product URL:
           <input
